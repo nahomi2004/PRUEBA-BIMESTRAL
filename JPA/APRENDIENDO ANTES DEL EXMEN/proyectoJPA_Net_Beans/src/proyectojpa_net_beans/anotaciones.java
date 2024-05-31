2341c8f043091fe18996b0fdadeafd8d39501c91
nahomi2004 <soy.nahomi.mucho.gusto@gmail.com>
2024-05-31 07:22:12 -0500

    anotaciones @

---
+++ JPA/APRENDIENDO ANTES DEL EXMEN/proyectoJPA_Net_Beans/src/proyectojpa_net_beans/anotaciones.java
@@ -0,0 +1,26 @@
+
+package proyectojpa_net_beans;
+
+/**
+ *
+ * @author N A H O M I
+ */
+public class anotaciones {
+    
+}
+
+/*
+ANOTACIONES MÁS USADAS
+
+@Entity
+@Id
+    @GenerateValue(strategy = GenerationType.SEQUENCE) ----> ID se genera automáticamente
+@Basic
+@Temporal
+    @Temporal(TemporalType.TIMESTAMP)
+    @Temporal(TemporalType.DATE)
+@OneToMany
+@ManyToMany
+@OneToOne
+@ManyToOne
+*/
